0e35264a58a97ee59464a61f2c76b23d3ea30f65
calebba <38850889+calebba@users.noreply.github.com>
2023-11-14 23:01:51 +0000

    Update 2023_10_13_175706_create_products_table.php

--- database/migrations/2023_10_13_175706_create_products_table.php
+++ database/migrations/2023_10_13_175706_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->decimal('cost', 10, 2);
+            $table->decimal('unit_price', 10, 2);
             $table->integer('quantity')->default(0);
             $table->timestamps();
         });
